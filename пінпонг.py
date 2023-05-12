0020ab0e128ca83a2a43d173a7cf302b95c511ef
cariexxstas <132276396+cariexxstas@users.noreply.github.com>
2023-05-12 17:24:04 +0300

    Add files via upload

--- пінпонг.py
+++ пінпонг.py
@@ -2,7 +2,7 @@ from pygame import*
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (weight, height))
+        self.image = transform.scale(image.load(player_image), (wight, height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -18,7 +18,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-    def update_1(self):
+    def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y >5:
             self.rect.y -= self.speed
@@ -37,7 +37,7 @@ clock = time.Clock()
 FPS = 60
 
 recket1 = Player('img.png', 30, 200, 4, 50, 150)
-recket1 = Player('img.png', 520, 200, 4, 50, 150)
+recket2 = Player('img.png', 520, 200, 4, 50, 150)
 ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
 
 font.init()
@@ -53,10 +53,41 @@ while game:
         if e.type == QUIT:
             game = False
 
-        if finish != True:
-            window.fill(back)
-            recket1.update_1()
-            recket2.update_r()
-            ball.rect.x += speed_x
-            ball.rect.y += speed_y
-            if 
+    if finish != True:
+        keys = key.get_pressed()
+        window.fill(back)
+        recket1.update_l()
+        recket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(recket1, ball) or sprite.collide_rect(recket2, ball):
+            speed_x *=-1
+            speed_y *=1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *=-1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            finish = False
+            if keys[K_r]:
+
+                ball.rect.x = 300
+                ball.rect.y = 250
+
+        if ball.rect.x > win_weight:
+            finish = True
+            window.blit(lose2, (200, 200))
+            finish = False
+            if keys[K_r]:
+
+                ball.rect.x = 300
+                ball.rect.y = 250
+
+        recket1.reset()
+        recket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
